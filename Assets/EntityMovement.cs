60c9df719f70124cfa0b73af80f799adf8b70c0a
William Convertino <williamconvertino@gmail.com>
2023-02-16 15:21:50 -0500

    Restructured the player controller

---
+++ Assets/EntityMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EntityMovement : MonoBehaviour
+{
+    #region Initialization
+    
+    private Rigidbody2D _rb2d;
+        
+    private void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    #endregion
+    
+    #region Movement
+    
+    [SerializeField] private float speed;
+
+    public Vector2 Direction { set; get; }
+    private void Update()
+    {
+        _rb2d.velocity = Direction * speed;
+    }
+
+    #endregion
+}
--- Assets/Scripts/Player/NetworkPlayerController.cs
+++ Assets/Scripts/Player/NetworkPlayerController.cs
@@ -1,33 +1,8 @@
 using Fusion;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 
-public class NetworkPlayerController : MonoBehaviour
+public class NetworkPlayerController : NetworkBehaviour
 {
     
-    #region Initialization
-    
-        private Rigidbody2D _rb2d;
-        
-        private void Start()
-        {
-            _rb2d = GetComponent<Rigidbody2D>();
-        }
-
-    #endregion
-    
-    #region Movement
-    
-    [SerializeField] private float speed;
-
-    private Vector2 _direction;
-    private void Update()
-    {
-        _direction = PlayerInput.GetMoveDirection().normalized;
-        _rb2d.velocity = _direction * speed;
-    }
-
-    #endregion
-
 }
